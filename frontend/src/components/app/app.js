e4bca5f7e69d64d3bf8b47105cfa0b0cc2deed2a
Mehrpuya <MehrpuyaFathi@gmail.com>
2022-07-09 19:46:35 +0200

    frontend , main app component

---
+++ frontend/src/components/app/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../../styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PeterPark from "../peterPark/peterPark";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <PeterPark />
+      </div>
+    );
+  }
+}
+
+export default App;
